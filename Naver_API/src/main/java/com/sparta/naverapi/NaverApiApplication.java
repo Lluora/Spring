7d36fbd02d4c84fce31b4ced33130881322be99b
Lluroa <luora@naver.com>
2021-11-10 01:07:21 +0900

    spring 3 계층 없이 구현하기

---
+++ Naver_API/src/main/java/com/sparta/naverapi/NaverApiApplication.java
@@ -0,0 +1,18 @@
+package com.sparta.naverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@SpringBootApplication
+@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
+@ServletComponentScan  // 3 layer 설정 후 주석 처리
+public class NaverApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverApiApplication.class, args);
+    }
+}
+
---
+++ Naver_API/src/main/java/com/sparta/naverapi/likeproduct/ProductMypriceRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.naverapi.likeproduct;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
