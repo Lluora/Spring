2b309c7dd8b623c54104512757e114540a3d47b7
Lluroa <luora@naver.com>
2021-11-11 23:18:10 +0900

    코드 중복제거 (Spring02-13)

--- Naver_API/src/main/java/com/sparta/naverapi/Controller/ProductController.java
+++ Naver_API/src/main/java/com/sparta/naverapi/Controller/ProductController.java
@@ -9,12 +9,24 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.SQLException;
 import java.util.List;
 
+import lombok.RequiredArgsConstructor; // 중복 삭제
+
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
+
+    // 멤버 변수 선언
+    private final ProductService productService;
+
+    public ProductController() {
+        // 멤버 변수 생성
+        productService = new ProductService();
+    }
+
+
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() throws SQLException {
-        ProductService productService = new ProductService();
+        //ProductService productService = new ProductService(); // 중복 삭제
         List<Product> products = productService.getProducts();
         // 응답 보내기
         return products;
@@ -23,7 +35,7 @@ public class ProductController {
     // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
+        //ProductService productService = new ProductService(); // 중복 삭제
         Product product = productService.createProduct(requestDto);
         // 응답 보내기
         return product;
@@ -32,7 +44,7 @@ public class ProductController {
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
+        //ProductService productService = new ProductService(); // 중복 삭제
         Product product = productService.updateProduct(id, requestDto);
         return product.getId();
     }
--- Naver_API/src/main/java/com/sparta/naverapi/Service/ProductService.java
+++ Naver_API/src/main/java/com/sparta/naverapi/Service/ProductService.java
@@ -9,16 +9,23 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class ProductService {
+    // 멤버 변수 선언
+    private final ProductRepository productRepository;
 
+    // 생성자: ProductService() 가 생성될 때 호출됨
+    public ProductService() {
+        // 멤버 변수 생성
+        this.productRepository = new ProductRepository();
+    }
 
 
     public List<Product> getProducts() throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
+        //ProductRepository productRepository = new ProductRepository(); //중복제거
         return productRepository.getProducts();
     }
 
     public Product createProduct(ProductRequestDto requestDto) throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
+        //ProductRepository productRepository = new ProductRepository(); //중복제거
         // 요청받은 DTO 로 DB에 저장할 객체 만들기
         Product product = new Product(requestDto);
         productRepository.createProduct(product);
@@ -26,7 +33,7 @@ public class ProductService {
     }
 
     public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
+        //ProductRepository productRepository = new ProductRepository(); //중복제거
         Product product = productRepository.getProduct(id);
         if (product == null) {
             throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
