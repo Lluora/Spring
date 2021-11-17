38b9b4ee5de92371cd89da4899331aa62d201211
Lluroa <luora@naver.com>
2021-11-18 02:24:22 +0900

    JPA 심화 - CRUD(02-07.)

--- 웹 개발의 봄/src/main/java/com/sparta/week02/Week02Application.java
+++ 웹 개발의 봄/src/main/java/com/sparta/week02/Week02Application.java
@@ -2,6 +2,7 @@ package com.sparta.week02;
 
 import com.sparta.week02.domain.Course;
 import com.sparta.week02.domain.CourseRepository;
+import com.sparta.week02.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,22 +20,88 @@ public class Week02Application {
     public static void main(String[] args) {
         SpringApplication.run(Week02Application.class, args);
     }
+//
+//    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+//    @Bean
+//    public CommandLineRunner demo(CourseRepository repository) {
+//        return (args) -> {
+//
+//            // 데이터 저장하기
+//            repository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+//
+//           // 데이터 전부 조회하기
+//            List<Course> courseList = repository.findAll();
+//            for (int i = 0; i < courseList.size(); i++) {
+//                Course course = courseList.get(i);
+//                System.out.println(course.getId());
+//                System.out.println(course.getTitle());
+//                System.out.println(course.getTutor());
+//            }
+//
+//             // 데이터 하나 조회하기
+//            Course course = repository.findById(1L).orElseThrow( //오류내보기 1L 대신 2L)
+//                    () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
+//                    //IllegalArgumentException 과 NullPointerException 차이?
+//                    // NullPointerException: 가리키는게 없을 때 (null을 가리키면)
 
-    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+
+//    // update 실행하기
+//    @Bean
+//    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
+//        return (args) -> {
+//            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+//
+//            System.out.println("데이터 인쇄");
+//            List<Course> courseList = courseRepository.findAll();
+//            for (int i = 0; i < courseList.size(); i++) {
+//                Course course = courseList.get(i);
+//                System.out.println(course.getId());
+//                System.out.println(course.getTitle());
+//                System.out.println(course.getTutor());
+//            }
+//
+//            Course new_course = new Course("웹개발의 봄, Spring", "임민영");
+//            courseService.update(1L, new_course);
+//            courseList = courseRepository.findAll();
+//            for (int i = 0; i < courseList.size(); i++) {
+//                Course course = courseList.get(i);
+//                System.out.println(course.getId());
+//                System.out.println(course.getTitle());
+//                System.out.println(course.getTutor());
+//            }
+//        };
+//    }
+
+
+    // delete
     @Bean
-    public CommandLineRunner demo(CourseRepository repository) {
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
         return (args) -> {
+            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
 
-            Course course1 = new Course("웹개발의 봄 Spring", "lulu");
-            repository.save(course1); // 저장
-
-            List<Course> courseList = repository.findAll(); //조회
-
-            for (int i=0; i<courseList.size(); i++) {
-                Course c = courseList.get(i);
-                System.out.println(c.getTitle());
-                System.out.println(c.getTutor());
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
             }
+
+            Course new_course = new Course("웹개발의 봄, Spring", "임민영");
+            courseService.update(1L, new_course);
+            courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            courseRepository.deleteAll();
         };
     }
-}
+
+
+};
+
