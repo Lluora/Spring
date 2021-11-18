c0d4599fb93c6b43a6a73e9cb0d2812143808486
Lluroa <luora@naver.com>
2021-11-18 17:19:30 +0900

    Lombok, DTO (02-08.)

--- 웹 개발의 봄/src/main/java/com/sparta/week02/Week02Application.java
+++ 웹 개발의 봄/src/main/java/com/sparta/week02/Week02Application.java
@@ -2,6 +2,7 @@ package com.sparta.week02;
 
 import com.sparta.week02.domain.Course;
 import com.sparta.week02.domain.CourseRepository;
+import com.sparta.week02.domain.CourseRequestDto;
 import com.sparta.week02.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,67 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
 
-@EnableJpaAuditing
-// Entity에 있어서 기본적으로 포함되는 createdAt, modifiedAt, createdBy, modifiedBy등을 자동으로 주입시킬 수있도록 해주는 설정
 
+@EnableJpaAuditing // Entity에 있어서 기본적으로 포함되는 것들을 자동으로 주입시킬 수있도록 해주는 설정
 @SpringBootApplication
 public class Week02Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Week02Application.class, args);
     }
-//
-//    // Week02Application.java 의 main 함수 아래에 붙여주세요.
-//    @Bean
-//    public CommandLineRunner demo(CourseRepository repository) {
-//        return (args) -> {
-//
-//            // 데이터 저장하기
-//            repository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
-//
-//           // 데이터 전부 조회하기
-//            List<Course> courseList = repository.findAll();
-//            for (int i = 0; i < courseList.size(); i++) {
-//                Course course = courseList.get(i);
-//                System.out.println(course.getId());
-//                System.out.println(course.getTitle());
-//                System.out.println(course.getTutor());
-//            }
-//
-//             // 데이터 하나 조회하기
-//            Course course = repository.findById(1L).orElseThrow( //오류내보기 1L 대신 2L)
-//                    () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
-//                    //IllegalArgumentException 과 NullPointerException 차이?
-//                    // NullPointerException: 가리키는게 없을 때 (null을 가리키면)
-
-
-//    // update 실행하기
-//    @Bean
-//    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
-//        return (args) -> {
-//            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
-//
-//            System.out.println("데이터 인쇄");
-//            List<Course> courseList = courseRepository.findAll();
-//            for (int i = 0; i < courseList.size(); i++) {
-//                Course course = courseList.get(i);
-//                System.out.println(course.getId());
-//                System.out.println(course.getTitle());
-//                System.out.println(course.getTutor());
-//            }
-//
-//            Course new_course = new Course("웹개발의 봄, Spring", "임민영");
-//            courseService.update(1L, new_course);
-//            courseList = courseRepository.findAll();
-//            for (int i = 0; i < courseList.size(); i++) {
-//                Course course = courseList.get(i);
-//                System.out.println(course.getId());
-//                System.out.println(course.getTitle());
-//                System.out.println(course.getTutor());
-//            }
-//        };
-//    }
-
 
     // delete
     @Bean
@@ -89,7 +37,8 @@ public class Week02Application {
             }
 
             Course new_course = new Course("웹개발의 봄, Spring", "임민영");
-            courseService.update(1L, new_course);
+            CourseRequestDto requestDto = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
+            courseService.update(1L, requestDto);
             courseList = courseRepository.findAll();
             for (int i = 0; i < courseList.size(); i++) {
                 Course course = courseList.get(i);
---
+++ 웹 개발의 봄/src/main/java/com/sparta/week02/domain/CourseRequestDto.java
@@ -0,0 +1,15 @@
+package com.sparta.week02.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+// Course 요청 관련 정보를 물고 다닐 녀석 → CourseRequestDto
+@Setter
+@RequiredArgsConstructor
+@Getter
+public class CourseRequestDto { //Course가 2개 가지고 있으므로 멤버면수 2개
+    private final String title;
+    private final String tutor;
+
+}
